7722581e4e276e1edb4cde1d857a8bc7db4aded1
Sandy <sandy@SandydeMacBook-Pro.local>
2022-10-14 10:07:13 +0800

    Feat: Add function that get data from HIS and send to iMWard

--- Flaskserver.py
+++ Flaskserver.py
@@ -1,20 +1,64 @@
+import json
 import logging
-import requests
-from flask import has_request_context, request
-from flask.logging import default_handler
+from progress.bar import Bar
+from datetime import datetime
 from flask import Flask, request, jsonify, redirect, url_for
+from flask_apscheduler import APScheduler
 import tokenrequests
 
+# logging.basicConfig(level=logging.DEBUG, filename='log.txt',
+#                     format='[%(asctime)s %(levelname)-8s] %(message)s',
+#                     datefmt='%Y%m%d %H:%M:%S',force=True)
+def Now_time():
+    return str(datetime.now().strftime("%Y/%m/%d %H:%M:%S.%f"))
+
+# define app
 app = Flask(__name__)
+
+# define auto scheduler
+scheduler = APScheduler()
+scheduler.api_enabled = True
+scheduler.init_app(app)
+
+# set scheduler
+
+
+@scheduler.task('interval', id='do_job_1', seconds=10)
+def DoRequestfromHIStoiMward():
+    # Take a header
+    print("["+Now_time()+" INFO ]Start update HIS to iMWard:")
+    header = tokenrequests.CreateHeader(tokenrequests.GetToken())
+
+    # Do request to getting area's bed_info from HIS
+    print("["+Now_time()+" INFO ]Geting data from HIS")
+    response_from_his = tokenrequests.GetdatafromHIS_request(header)
+
+    # Do request to update bed_info to iMward
+    print("["+Now_time()+" INFO ]Do update to iMWard")
+    bar = Bar('Processing', max=len(response_from_his['data']))
+    # Do Data Converter and request update to iMward
+    for i in response_from_his['data']:
+        body = tokenrequests.HIStoiMward_Dataconverter(i)
+        respons = tokenrequests.UpdateiMward_request(body)
+        if respons != str(200):
+            print("["+Now_time()+" ERROR ]Update to iWard occured Error")
+        logging.info("Bed number:"+i['bedNum']+" Update response is : "+respons)
+        bar.next()
+        print("\n")
+    bar.finish()
+    print("["+Now_time()+" INFO ]Finish Bed iMWard info Update")
+
+
+# set Flask service router
 
 
 @app.route("/", methods=['POST'])
 def getimvs():
     content = request.get_json(force=True)
-    #print("get imvs contents:", content)
-    #print("Do request to HIS")
-    res = tokenrequests.Dorequest(content)
-    # print(res.text)
+    logging.info("iMVS data is:"+json.dumps(content))
+    print("["+Now_time()+" INFO ]Access iMVSdata, Do request to HIS")
+    res = tokenrequests.UpdateHIS_request(content)
+    print("["+Now_time()+" INFO ]request from HIS : "+res.text)
     return res.text
 
 
@@ -28,5 +72,5 @@ def all_routes(text):
 
 if __name__ == "__main__":
     app.config['JSON_AS_ASCII'] = False
-    logging.basicConfig(filename='error.log', level=logging.DEBUG)
-    app.run(host='0.0.0.0', port=8080)
+    scheduler.start()
+    app.run(host='0.0.0.0', port=8070)
--- tokenrequests.py
+++ tokenrequests.py
@@ -1,18 +1,57 @@
 import json
+from datetime import datetime
+from wsgiref import headers
 import requests
+import logging
 
-link = 'Noibo.shingmark'
+# Config link Path
+# His_link = 'Noibo.shingmark'
+His_link = '115.75.7.105'
+iMward_link = '127.0.0.1'
+
+# setting logging config
+logging.basicConfig(level=logging.DEBUG, filename='request.log', filemode='w',
+                    format='[%(asctime)s %(levelname)-8s] %(message)s',
+                    datefmt='%Y%m%d %H:%M:%S')
+
+def UpdateiMward_request(data):
+    # logging.info("UpdateiMward_request header is:"+header)
+    header = {"Content-type": "application/json"}
+    logging.info("UpdateiMward_request Data is:"+json.dumps(data['datalist'][0]['bedNum']))
+    try:
+        # response2 = requests.post(
+        #     "http://" + iMward_link + ":5000/InsertBedInfo", headers=header, json=data)
+        response2 = requests.post(
+            "http://"+iMward_link+":5000/UpdateBedInfo", headers=header, json=data)
+    except Exception as error:
+        logging.error(str(error))
+        return 'Request Error'
+    return str(response2.status_code)
 
 
-def Dorequest(data):
-    token = GetToken()
+def GetdatafromHIS_request(header):
+    body = {'module': 'department_info',
+            'value': {'Khoa': 'KUB'}}
+    response2 = requests.post(
+        "http://"+His_link+":9090/api/v1/", headers=header, json=body)
+    return response2.json()
+
+
+def UpdateHIS_request(data):
+    headers = CreateHeader(GetToken())
+    con_data = iMVStoHIS_DataConverter(data)
+    logging.info("UpdateHIS_request header is:"+json.dumps(headers))
+    logging.info("UpdateHIS_request Data is:"+json.dumps(con_data))
+    #print("To HIS:", con_data)
+    response2 = requests.post(
+        "http://"+His_link+":9090/api/v1/", headers=headers, json=con_data)
+    return response2
+
+
+def CreateHeader(token):
     headers = {"Content-type": "application/json",
                "Authorization": "Bearer " + token}
-    con_data = DataConverter(data)
-    print("To HIS:", con_data)
-    response2 = requests.post(
-        "http://"+link+":9090/api/v1/", headers=headers, json=con_data)
-    return response2
+    return headers
 
 
 def GetToken():
@@ -20,13 +59,19 @@ def GetToken():
     user = {"username": "api_user1",
             "userpassword": "óøùþñäõüõóoýÏñàù"}  # change config
     response = requests.post(
-        "http://"+link+":9090/api/v1/login/", headers=headers, json=user)
+        "http://"+His_link+":9090/api/v1/login/", headers=headers, json=user)
     token = response.json()["token"]
-    print("token:", token)
     return token
 
+def DateConverter(Originaltime):
+    if Originaltime == "":
+        return Originaltime
+    else:
+        time = datetime.strptime(Originaltime[:-3], '%m/%d/%Y %H:%M:%S')
+        New_time = time.strftime("%Y-%m-%d")
+        return New_time
 
-def DataConverter(Originaldata):
+def iMVStoHIS_DataConverter(Originaldata):
     newdata = {'module': 'life_function',
                'value': {
                    'patientNo': Originaldata['patientNo'],
@@ -35,3 +80,38 @@ def DataConverter(Originaldata):
                    'measureItems': Originaldata['measureItems']
                }}
     return newdata
+
+
+def HIStoiMward_Dataconverter(Originaldata):
+    newData = {
+        "datalist": [{"patientName": Originaldata['patientName'],
+                      "age":Originaldata['age'],
+                      "birthDate":DateConverter(Originaldata['birthDate']),
+                      "gender":Originaldata['gender'],
+                      "bedNum":Originaldata['bedNum'],
+                      "inPatientNum":Originaldata['inPatientNum'][0:20],
+                      "mainDoc":Originaldata['mainDoc'][0:16],
+                      "nurse":Originaldata['nurse'][0:16],
+                      "nurse2":Originaldata['nurse2'],
+                      "inDoc":Originaldata['inDoc'][0:16],
+                      "precaution":Originaldata['precaution'],
+                      "inDepartDate":DateConverter(Originaldata['inDepartDate']),
+                      "outDepartDate":DateConverter(Originaldata['outDepartDate']),
+                      "nursingLevel":Originaldata['nursingLevel'],
+                      "area":Originaldata['area'],
+                      "operationDate":Originaldata['operationDate'],
+                      "property1":Originaldata['property1'],
+                      "property2":Originaldata['property2'],
+                      "property3":Originaldata['property3'],
+                      "property4":Originaldata['property4'],
+                      "property5":Originaldata['property5'],
+                      "diastolicBloodPressure":Originaldata['diastolicBloodPressure'],
+                      "systolicBloodPressure":Originaldata['systolicBloodPressure'],
+                      "bodyTemperature":Originaldata['bodyTemperature'],
+                      "height":Originaldata['height'],
+                      "weight":Originaldata['weight']
+                      }],
+        "tenant_nammme": "t1"
+    }
+
+    return newData
